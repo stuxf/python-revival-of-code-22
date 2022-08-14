0af6dc94dd5f80adb1973c7e8881cd333a5bba07
user <70670632+stuxf@users.noreply.github.com>
2022-08-14 01:20:28 -0700

    update to be more pythonic to use enumerate

--- day-1/NotQuiteLisp.py
+++ day-1/NotQuiteLisp.py
@@ -1,20 +1,16 @@
-# open input.txt and read it
 with open('input.txt') as f:
     data = f.read()
 
-# split the data into a list of chars
-input = list(data)
 floor = 0
-counter = 0
-reached_below_basement = False
-for i in input:
-    if floor == -1:
-        reached_below_basement = True
-    elif not reached_below_basement:
-        counter += 1
-    if i == '(':
+basement_counter = None
+
+for count, instruction in enumerate(data):
+    if floor == -1 and basement_counter is None:
+        basement_counter = count
+    if instruction == '(':
         floor += 1
-    elif i == ')':
+    elif instruction == ')':
         floor -= 1
-print(floor)
-print(counter)
+
+print(f"Santa ends up on floor {floor}")
+print(f"Santa enters the basement on instruction {basement_counter}")
